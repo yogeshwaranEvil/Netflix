6e065cc8fda0f9861e11d6ba700c56053f3b9309
yogeshwaranEvil <yogeshwaran.r2022lcse.sece.ac.in>
2024-03-14 09:18:36 +0530

    Add 3

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    strictPort: true,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'https://my-app.yogeshwaran-r2022lcse.workers.dev',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  
   plugins: [react()],
 })
